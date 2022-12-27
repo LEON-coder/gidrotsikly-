8df11804315bc4f3e334cb99d5a6c8c4ebbbbc40
Leonid <leonid.spb2015@yandex.ru>
2022-12-28 00:13:03 +0300

    Добавление выпадающего меню с чекбоксми на странице каталога

--- build/js/like.js
+++ build/js/like.js
@@ -1,12 +1,4 @@
-/*
-let likeIcon = document.querySelector('.like');
-likeIcon.addEventListener('click', likeChange);
-function likeChange() {
-  likeIcon.classList.toggle('like--active');  
-}
-*/
 $(document).ready(function(){
-
       $('.like').on('click', function() {
         $(this).toggleClass('like--active');
       });
