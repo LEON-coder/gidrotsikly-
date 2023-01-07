ca0abcdd38aec6132be27a429dab82095d6d1702
Leonid <leonid.spb2015@yandex.ru>
2023-01-07 20:47:37 +0300

    Сделал на 95% страницу каталога для разрешения мобильного экрана (до 450 px) , добавил функцию открывания карточек товара по 3 штуки для кнопки 'Показать еще'

---
+++ build/js/load-more.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+    $(".catalog-content").slice(0, 3).show();
+    $("#loadMore").on("click", function(e){
+      e.preventDefault();
+      $(".catalog-content:hidden").slice(0, 3).slideDown();
+      if($(".catalog-content:hidden").length == 0) {
+        $("#loadMore").text("Больше нет").addClass("Все показано");
+      }
+    });
+    
+  })
