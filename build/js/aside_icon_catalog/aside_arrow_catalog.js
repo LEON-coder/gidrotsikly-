781a26451195eaa2406f858af26a7e0252f1fe3f
Leonid <leonid.spb2015@yandex.ru>
2023-01-08 16:18:16 +0300

    Верстка в блоке 'Block-gydrocycles__block-headline' блока выбора настроек (по популярности), а также анимировал стрелки у пунктов меню боковой панели aside ('Наличие' , 'Новинки ' и т.д)

---
+++ build/js/aside_icon_catalog/aside_arrow_catalog.js
@@ -0,0 +1,55 @@
+let ButtonArrow = document.querySelector(".button--arrow");
+let ArrowDown = document.querySelector(".icon_headline");
+
+ButtonArrow.addEventListener("click", function() {    
+    ArrowDown.classList.toggle("icon_reverse");       
+});
+
+let ArrowNew = document.querySelector(".button-arrow-new");
+let ArrowDownNew = document.querySelector(".icon--new");
+
+ArrowNew.addEventListener("click", function() {    
+    ArrowDownNew.classList.toggle("icon_reverse");       
+});
+
+let ButtonArrowPrice = document.querySelector(".button-arrow-price");
+let ArrowDownPrice = document.querySelector(".icon--price");
+
+ButtonArrowPrice.addEventListener("click", function() {    
+    ArrowDownPrice.classList.toggle("icon_reverse");       
+});
+
+
+let ButtonArrowBrand = document.querySelector(".button-arrow-brand");
+let ArrowDownBrand = document.querySelector(".icon--brand");
+
+ButtonArrowBrand.addEventListener("click", function() {    
+    ArrowDownBrand.classList.toggle("icon_reverse");       
+});
+
+
+
+let ButtonArrowModel = document.querySelector(".button-arrow-model");
+let ArrowDownModel = document.querySelector(".icon--model");
+
+ButtonArrowModel.addEventListener("click", function() {    
+    ArrowDownModel.classList.toggle("icon_reverse");       
+});
+
+
+let ButtonArrowStocks = document.querySelector(".button-arrow-stocks");
+let ArrowDownStocks = document.querySelector(".icon--stocks");
+
+ButtonArrowStocks.addEventListener("click", function() {    
+    ArrowDownStocks.classList.toggle("icon_reverse");       
+});
+
+
+let ButtonArrowCountries = document.querySelector(".button-arrow-countries");
+let ArrowDownCountries = document.querySelector(".icon--countries");
+
+ButtonArrowCountries.addEventListener("click", function() {    
+    ArrowDownCountries.classList.toggle("icon_reverse");       
+});
+
+
