1c5c8182010774cb521a54db3725b9b4da874301
Leonid <leonid.spb2015@yandex.ru>
2023-11-22 21:10:04 +0300

    Корректтровка открытия и закрытия бургер-меню

--- build/js/mobile-menu.js
+++ build/js/mobile-menu.js
@@ -1,10 +1,12 @@
 let btnMenu = document.querySelector(".mobile_icon-burger");
 let BurgerMenu = document.querySelector(".mobile-menu");
+let CloseBurger = document.getElementsByTagName("body");
 
-btnMenu.addEventListener("click", function() {   
+btnMenu.addEventListener("click",function () {
     BurgerMenu.classList.toggle("show-menu");
     function animation() {
         document.getElementsByClassName('mobile-menu').className = 'show-menu'
     }
     btnMenu.classList.toggle("burger--active");
+    CloseBurger.classList.remove("burger--active");
 })
