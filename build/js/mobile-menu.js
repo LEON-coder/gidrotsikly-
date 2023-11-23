4044e76a6ab79ec3b7fd63dedcb869ac5aeac441
Leonid <leonid.spb2015@yandex.ru>
2023-11-23 21:40:50 +0300

    Корректировка мобильного бургер-меню

--- build/js/mobile-menu.js
+++ build/js/mobile-menu.js
@@ -1,22 +1,16 @@
 let btnMenu = document.querySelector(".mobile_icon-burger");
-let BurgerMenu = document.querySelector(".mobile-menu");
-let CloseBurger = document.getElementsByTagName("body");
+let BurgerMenu = document.querySelector(".header__list_mobile-background");
+
+
 
 btnMenu.addEventListener("click",function () {
     BurgerMenu.classList.add("show-menu");
     function animation() {
-        document.getElementsByClassName('mobile-menu').className = 'show-menu'
+        document.BurgerMenu.className = 'show-menu'
     }
     btnMenu.classList.toggle("burger--active");
 
 });
-CloseBurger.addEventListener("click",function () {
-    BurgerMenu.classList.remove("show-menu");
-    function animation() {
-        document.getElementsByClassName('mobile-menu').className = 'show-menu'
-    }
-    btnMenu.classList.remove("burger--active");
 
-});
 
 
