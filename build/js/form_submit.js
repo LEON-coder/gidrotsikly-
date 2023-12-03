018784d4c48ba35923c804536a477e172a5e57fb
Leonid <leonid.spb2015@yandex.ru>
2023-12-03 18:49:36 +0300

    Мелкие правки верстки

---
+++ build/js/form_submit.js
@@ -0,0 +1,14 @@
+
+let form = document.querySelector(".footer__describe-area-block");
+let inputEmail = document.querySelector(".js-input-email");
+
+form.onsubmit = function (event) {
+	event.preventDefault();
+
+	let emailVal = inputEmail.value
+
+	if (emailVal === '') {
+		inputEmail.classList.add('error');
+	}
+	return false;
+}
