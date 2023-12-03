f3c125019337de1eabcee48eaa44be0bc906177f
Leonid <leonid.spb2015@yandex.ru>
2023-12-03 19:36:07 +0300

    Мелкие правки верстки

--- build/js/form_submit.js
+++ build/js/form_submit.js
@@ -4,7 +4,6 @@ let inputEmail = document.querySelector(".js-input-email");
 
 form.onsubmit = function (event) {
 	event.preventDefault();
-
 	let emailVal = inputEmail.value
 
 	if (emailVal === '') {
